6cdba2bf22a64554cee122ed54a03437c11beb24
OfekKadir <ofekkadir9@gmail.com>
2025-10-29 14:30:38 +0200

    commpds

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,12 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name2 = args[1];
+	    String name3 = args[2];
+		int amount = Integer.parseInt(args[3]);  // Total bill amount
+		double share = Math.ceil(amount / 3.0);    // Each diner's share of the bill
+		System.out.println("Dear "+ name3 + ", " + name2 +
+						   ", and " + name1 + ": pay " +
+						    share + " Shekels each.");
 	}
 }
